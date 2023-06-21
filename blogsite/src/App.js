4459e2ec0534bb39a1aa129d62b89f7708edf138
Abdullah Mehmood Minhas <aminhas309@hotmail.com>
2023-06-22 02:47:17 +0500

    Building React Environment with MUI

---
+++ blogsite/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+   <>
+   <Button variant='contained'>
+    Press me eh
+   </Button>
+   </>
+  );
+}
+
+export default App;
